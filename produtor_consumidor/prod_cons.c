ab459da9895f2d6bf1bc4ae5661a32237b181c22
Andre Borges <andrelbol@yahoo.com.br>
2017-09-24 09:44:48 -0300

    Adicionados alguns pormenores na produtor_consumidor

--- produtor_consumidor/prod_cons.c
+++ produtor_consumidor/prod_cons.c
@@ -6,79 +6,80 @@
 #include "queue.h"
 
 #define MAX_ITEMS 5
+#define NUM_PRODUTORS 3
+#define NUM_CONSUMERS 2
+#define RAND_ITEM_ATTR 1000 // Parameter to get a 0...999 item
+#define RAND_SLEEP_ATTR 4 // Parameter to get a 0...3 sleep time
 
-// Queue
-typedef struct filaint_t
+// Queue Struct
+typedef struct int_queue_t
 {
-  struct filaint_t *prev ;  // ptr para usar cast com queue_t
-  struct filaint_t *next ;  // ptr para usar cast com queue_t
+  struct int_queue_t *prev ;  // ptr para usar cast com queue_t
+  struct int_queue_t *next ;  // ptr para usar cast com queue_t
   int id ;
   // outros campos podem ser acrescidos aqui
-} filaint_t ;
-filaint_t *buffer;
-filaint_t item[MAX_ITEMS];
+} int_queue_t ;
+
+int_queue_t *buffer;
 
 // Semaphores
 sem_t *s_buffer, *s_item, *s_vaga;
-
-// Index of vector of ints
-int i = 0;
-
-
-void print_elem (void *ptr)
-{
-  filaint_t *elem = ptr ;
-
-  if (!elem)
-  return ;
-
-  elem->prev ? printf ("%d", elem->prev->id) : printf ("*") ;
-  printf ("<%d>", elem->id) ;
-  elem->next ? printf ("%d", elem->next->id) : printf ("*") ;
-}
-
 
 // Produtor e Consumidor
 
 void produtor(){
   int pause;
+  int_queue_t *item;
   while(1){
-    pause = rand()%4;
+    pause = rand()%RAND_SLEEP_ATTR;
     sleep(pause);
+
+    // Enter critical session
     sem_wait(&s_vaga);
     sem_wait(&s_buffer);
-    // Adding item
-    item[i].id = rand()%1000;
-    item[i].prev = NULL;
-    item[i].next = NULL;
 
-    queue_append((queue_t **) &buffer, (queue_t *) item[i]);
+    // Adding item
+    item = (int_queue_t *) malloc (sizeof(int_queue_t));
+    item->id = rand()%RAND_ITEM_ATTR;
+    item->prev = NULL;
+    item->next = NULL;
+    queue_append((queue_t **) &buffer, (queue_t *) item);
+
+    // Leave critical session
     sem_post(&s_buffer);
     sem_post(&s_item);
-    print_elem(item[i]);
-    i++;
+
+    // Print result
+    printf("A Thread X produziu ");
+    print_elem(item);
   }
 }
 
 void consumidor(){
   int pause;
-  i=0;
-  filaint_t aux = buffer;
+  int_queue_t *aux;
   while(1){
+    // Enter critial session
     sem_wait(&s_item);
     sem_wait(&s_buffer);
+
+    // Remove item
+    aux = buffer->prev; // Get the last elem of the queue
     queue_remove((queue_t **) &buffer, (queue_t *) aux);
+
+    // Leave critical session
     sem_post(&s_buffer);
     sem_post(&s_vaga);
-    print_elem(aux;)
-    pause = rand()%4;
+    print_elem(aux);
+    pause = rand()%RAND_SLEEP_ATTR;
     sleep(pause);
   }
 }
 
 void print_elem (void *ptr)
+// Given an element, print it on the screen
 {
-  filaint_t *elem = ptr ;
+  int_queue_t *elem = ptr ;
 
   if (!elem)
   return ;
@@ -91,5 +92,14 @@ void print_elem (void *ptr)
 //Main
 
 int main(){
+  // Initializing semaphores
+  // Buffer semaphore is binary
+  sem_init(&s_buffer, 0, 1);
+
+  // Vacancy semaphore can have MAX_ITEMS items
+  sem_init(&s_vaga, 0, MAX_ITEMS);
+
+  // Item semaphore starts nulled
+  sem_init(&s_item, 0, 0);
   return 0;
 }
